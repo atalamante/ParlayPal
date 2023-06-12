388695b3647eed23eb8a04e7d9570440f86fe6b7
atalamante <atalamante@unm.edu>
2023-06-12 17:47:13 -0600

    This commit is focused around splitting the screen to display the current parlay being built. Initially, the UI looks the same with the game slates for each sport taking up the whole screen. However, when you click on the first game in your future parlay, the schedule will go to 70% of the screen while the remaining 30% will display the parlay you are building. It is using the same card format as the schedule and the games are added as you click on more. It does have a check to make sure you are not adding duplicate games. Next, I want to add some kind of functionality that allows you to remove a game, clear the list, and submit the parlay.

--- public/espnData.js
+++ public/espnData.js
@@ -5,31 +5,7 @@ pathForNHLScores = "http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scor
 
 pathToFetch = "localhost:5500/fetch";
 
-// async function getScores(pathForSport) {
-//     currentDateInFormat = getDate();
-//     let response = await fetch(pathForSport + currentDateInFormat);
-//     let data = await response.json();
-//     console.log(data.events);
-//     return data.events;
-// }
-
-// function getDate() {
-//     var today = new Date()
-//     var dd = String(today.getDate()).padStart(2, '0');
-//     var mm = String(today.getMonth() + 1).padStart(2, '0');
-//     var yyyy = today.getFullYear();
-//     // Proper Format: yyyymmdd
-//     properDateFormat = yyyy + mm + dd;
-//     return properDateFormat;
-// }
-
-// async function parseGameSlates(sportSpecificPath, sportSpecificList) {
-//     sportGameSlateInfo = await getScores(sportSpecificPath);
-//     for (var game = 0; game < sportGameSlateInfo.length; game++) {
-//         sportSpecificList.push(createGameObjects(sportGameSlateInfo[game]));
-//     }
-//     console.log(sportSpecificList);
-// }
+const parlay = [];
 
 // function callAPISpecificTime() {
 //     var now = new Date();
@@ -40,79 +16,85 @@ pathToFetch = "localhost:5500/fetch";
 //     setTimeout(function () {
 //         alert("It's 11:22!")
 //     }, millisTill12);
-
 // }
 
-// TODO: Read in the games into object format where a single game object contains: team1, team2, spread, time. 
-// Eventually, post these games to database. For now, need to just get an array containing all game objects.
-// Once we have all the games, need to populate "game cards" inside the tabs. 
-
-// function createGameObjects(currGame) {
-//     if (currGame.competitions[0].hasOwnProperty('odds')) {
-//         spreadHolder = currGame.competitions[0].odds[0].details;
-//     } else {
-//         spreadHolder = "-----";
-//     }
-//     gametime = new Date(currGame.date);
-//     switch (currGame.status.type.id) {
-//         case "1": // Scheduled Game. Hasn't started yet.
-//             var currGameObject = {
-//                 teamHome: currGame.competitions[0].competitors[0].team.displayName, teamAway: currGame.competitions[0].competitors[1].team.displayName,
-//                 spread: spreadHolder, teamHomeScore: 0, teamAwayScore: 0, time: gametime.toLocaleTimeString('en-US'), shortName: currGame.shortName, gameID: currGame.status.type.id
-//             };
-//             break;
-//         case "2": // Live Game
-//             homeScore = currGame.competitions[0].competitors[0].score;
-//             awayScore = currGame.competitions[0].competitors[1].score;
-//             console.log(awayScore);
-//             console.log(homeScore);
-//             var currGameObject = {
-//                 teamHome: currGame.competitions[0].competitors[0].team.displayName, teamAway: currGame.competitions[0].competitors[1].team.displayName,
-//                 spread: spreadHolder, teamHomeScore: homeScore, teamAwayScore: awayScore, time: gametime.toLocaleTimeString('en-US'), shortName: currGame.shortName, gameID: currGame.status.type.id
-//             };
-//             break;
-//         case "3": // Completed. 
-//             homeScore = currGame.competitions[0].competitors[0].score;
-//             awayScore = currGame.competitions[0].competitors[1].score;
-//             var currGameObject = {
-//                 teamHome: currGame.competitions[0].competitors[0].team.displayName, teamAway: currGame.competitions[0].competitors[1].team.displayName,
-//                 spread: spreadHolder, teamHomeScore: homeScore, teamAwayScore: awayScore, time: gametime.toLocaleTimeString('en-US'), shortName: currGame.shortName, gameID: currGame.status.type.id
-//             };
-//             break;
-//         // case "4":
-//         //     gameScore.innerText = "Forfeit";
-//         // case "5":
-//         //     gameScore.innerText = "Cancelled";
-//         // case "6":
-//         //     gameScore.innerText = "Postponed";
-//         // case "7":
-//         //     gameScore.innerText = "Delayed";
-//         // case "8":
-//         //     gameScore.innerText = "Suspended";
-//         default:
-//             if (currGame.competitions[0].competitors[0].hasOwnProperty('score')) {
-//                 homeScore = currGame.competitions[0].competitors[0].score;
-//                 awayScore = currGame.competitions[0].competitors[1].score;
-//                 var currGameObject = {
-//                     teamHome: currGame.competitions[0].competitors[0].team.displayName, teamAway: currGame.competitions[0].competitors[1].team.displayName,
-//                     spread: spreadHolder, teamHomeScore: homeScore, teamAwayScore: awayScore, time: gametime.toLocaleTimeString('en-US'), shortName: currGame.shortName, gameID: currGame.status.type.id
-//                 };
-//             } else {
-//                 var currGameObject = {
-//                     teamHome: currGame.competitions[0].competitors[0].team.displayName, teamAway: currGame.competitions[0].competitors[1].team.displayName,
-//                     spread: spreadHolder, teamHomeScore: "0", teamAwayScore: "0", time: gametime.toLocaleTimeString('en-US'), shortName: currGame.shortName, gameID: currGame.status.type.id
-//                 };
-//             }
-//             break;
-//     }
-//     return currGameObject;
-// }
 
 async function makeCallToFetchX(sport) {
     let response = await fetch('/fetch' + sport);
     let data = await response.json();
     console.log("Data: ", data);
     return data;
+}
+
+function updateParlayDisplay() {
+    const parlayContainer = document.querySelector(".parlay-container");
+    console.log("Parlay: ", parlay);
+
+    for (const currGame of parlay) {
+        let game = currGame[0];
+        console.log("Game Stat: ", currGame[1]);
+
+        if (currGame[1] === 0) {
+            currGame[1] = 1;
+        } else {
+            continue;
+        }
+        const parlayItem = document.createElement('div');
+        parlayItem.classList.add("card");
+        if (game.gameID == "1") {
+            parlayItem.classList.add("scheduled");
+        } else if (game.gameID == "2") {
+            parlayItem.classList.add("active");
+        } else if (game.gameID == "3"){
+            parlayItem.classList.add("finished");
+        }
+
+        const nameOfTeams = document.createElement("h2");
+        nameOfTeams.classList.add("team-names");
+        nameOfTeams.textContent = `${game.awayTeam} vs ${game.homeTeam}`;
+
+        const spread = document.createElement("p");
+        spread.classList.add("spread");
+        spread.textContent = `Spread: ${game.spread}`;
+
+        const gameTime = document.createElement("p");
+        gameTime.classList.add("time");
+        gameTime.textContent = `Time: ${game.startTime}`;
+
+        const currScore = document.createElement("p");
+        currScore.classList.add("score");
+        if (game.gameID == "2" || game.gameID == "3") {
+            currScore.textContent = `Score: ${game.awayTeamScore} - ${game.homeTeamScore}`;
+        } else {
+            currScore.textContent = "";
+        }
+
+        parlayItem.appendChild(nameOfTeams);
+        parlayItem.appendChild(currScore);
+        parlayItem.appendChild(spread);
+        parlayItem.appendChild(gameTime);
+        
+        parlayContainer.appendChild(parlayItem);
+    }
+}
+
+function addToParlay(selectedGame) {
+    const parlayContainer = document.querySelector(".parlay");
+    const scheduleContainer = document.querySelector(".schedule");
+
+    // Check to see if selectedGame is already in parlay!
+    let gameIndex = parlay.findIndex((item) => item[0].shortName === selectedGame.shortName);
+    if (gameIndex == -1) {
+        // Game is NOT in parlay. Add it!
+        parlay.push([selectedGame, 0]);
+
+        if (parlay.length != 0) {
+            parlayContainer.style.display = 'grid';
+            scheduleContainer.style.width = '70%';
+        }
+
+        updateParlayDisplay();
+    }
 }
 
 function dynamicGameCards(sportSpecificList, sport) {
@@ -162,6 +144,7 @@ function dynamicGameCards(sportSpecificList, sport) {
             const startTime = game.startTime;
 
             console.log(`Clicked card: ${awayTeam} vs ${homeTeam}, Spread: ${spread}, Start Time: ${startTime}`, `Score: ${game.awayTeamScore} - ${game.homeTeamScore}`);
+            addToParlay(game);
         })
 
         containerForGameCards.appendChild(singleCard);
@@ -227,29 +210,5 @@ function tabEventHandlingSetup() {
 }
 
 const NBAGameList = [];
-// const MLBGameList = [];
-// const NHLGameList = [];
-// const WNBAGameList = [];
-// const CBBGameList = [];
-// parseGameSlates(pathForNBAScores, NBAGameList);
-// parseGameSlates(pathForMLBScores, MLBGameList);
-// parseGameSlates(pathForNHLScores, NHLGameList);
-// parseGameSlates(pathForWNBAScores, WNBAGameList);
 tabEventHandlingSetup();
 // callAPISpecificTime();
-
-function testServer() {
-    $.ajax({
-        url: '/fetchNBA',
-        method: 'GET',
-        success: function (data) {
-            console.log(data);
-            console.log("SUCCESS");
-        },
-        error: function (error) {
-            console.error('Error', error);
-        }
-    });
-}
-
-// testServer();
